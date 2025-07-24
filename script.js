55929d2b325ab0832334a9e625ee0d0aacea72f2
Guilherme Devito <117872245+Devito552@users.noreply.github.com>
2025-07-24 14:32:58 -0300

    24/07 14:30

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+let clienteWeb = null;
 
-// Estado das lâmpadas
+const clientId = 'Esp32' + Math.floor(Math.random() * 900) + 100;
+clienteWeb = new Paho.MQTT.Client('broker.hivemq.com', 8884, clientId);
+
+clienteWeb.connect({   
+    useSSL: true, 
+    onSuccess: function() {
+        alert('A conexão com Broker foi bem sucedida')
+    },
+    onFailure: function() {
+        alert('A conexão com Broker falhou')
+    }
+});
+
 const estadoLampadas = {
     sala: false,
     cozinha: false,
@@ -17,12 +30,23 @@ function toggleLampada(comodo) {
         lampada.classList.add('ligada');
         botao.textContent = 'Desligar';
         botao.classList.add('desligado');
+        
+        // Criar mensagem MQTT para ligar
+        const msg = new Paho.MQTT.Message("1");
+        msg.destinationName = `senai928/lampada/${comodo}/ligar`;
+        clienteWeb.send(msg);
     } else {
         lampada.classList.remove('ligada');
         botao.textContent = 'Ligar';
         botao.classList.remove('desligado');
+        
+        // Criar mensagem MQTT para desligar
+        const msg = new Paho.MQTT.Message("0");
+        msg.destinationName = `senai928/lampada/${comodo}/desligar`;
+        clienteWeb.send(msg);
     }
 
-    // Aqui você pode adicionar código para comunicar com o ESP32
     console.log(`Lâmpada da ${comodo}: ${estadoLampadas[comodo] ? 'ligada' : 'desligada'}`);
 }
+
+
